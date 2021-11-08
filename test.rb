d16777328a7c0960504d581e098c3b5d980ffda7
Tom Zhang <tom.zhang989@gmail.com>
2021-11-08 15:09:27 -0500

    more additions to test requirements

--- test.rb
+++ test.rb
@@ -1,14 +1,19 @@
+# # STARTUP
 # When starting up should ask for an input number to initalize questions
 
 # When a number is input, should display a question
 
 # When starting up should ask the player who's turn it is to enter
 
+## ANSWERING QUESTIONS
 # when a player enters a response should check response against
 # stored/generated answer
 
+
+## RESPONDING TO QUESTIONS
 # After response is checked should return a confirmation statement
 
 # After confirmation statement is given, should pass turn to next player
 
+## Concluding Game
 # When a player drops to <1 lives should conclude the game and end program
